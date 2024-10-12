e5a6f946e927439466f2cc8ec2d9033fc2e7a235
Mark Golovatskikh <m.golovatskih@gmail.com>
2024-10-12 15:01:32 +0200

    add image scaling

--- Image.h
+++ Image.h
@@ -14,6 +14,9 @@ public:
             std::cout << "Failed to load image "
                       << File << ":\n  " << SDL_GetError();
         }
+        d_sourceRectangle.w = ImageSurface->w;
+        d_sourceRectangle.h = ImageSurface->h;
+        SetDestinationRectangle({0, 0, 600, 300});
         if (p_preferredFormat)
         {
             auto *converted = SDL_ConvertSurface(ImageSurface, p_preferredFormat, 0);
@@ -31,7 +34,7 @@ public:
 
     void Render(SDL_Surface *DestinationSurface)
     {
-        SDL_BlitSurface(ImageSurface, nullptr, DestinationSurface, nullptr);
+        SDL_BlitScaled(ImageSurface, &d_sourceRectangle, DestinationSurface, &d_destinationRectangle);
     }
 
     ~Image()
@@ -43,5 +46,38 @@ public:
     Image &operator=(const Image &) = delete;
 
 private:
-    SDL_Surface *ImageSurface{nullptr};
+    SDL_Surface *ImageSurface;
+    SDL_Rect d_sourceRectangle{0, 0, 0, 0};
+    SDL_Rect d_destinationRectangle{0, 0, 0, 0};
+
+    void SetDestinationRectangle(
+        SDL_Rect Requested)
+    {
+        float TargetRatio{d_sourceRectangle.w / static_cast<float>(d_sourceRectangle.h)};
+        float RequestedRatio{Requested.w / static_cast<float>(Requested.h)};
+
+        d_destinationRectangle = Requested;
+
+        if (RequestedRatio < TargetRatio)
+        {
+            // Reduce height
+            d_destinationRectangle.h = static_cast<int>(
+                Requested.w / TargetRatio);
+        }
+        else
+        {
+            // Reduce width as before
+            d_destinationRectangle.w = static_cast<int>(
+                Requested.h * TargetRatio);
+        }
+
+        // Non-functional code for logging
+        float AppliedRatio{d_destinationRectangle.w /
+                           static_cast<float>(d_destinationRectangle.h)};
+
+        std::cout << "\n[Aspect Ratio] Requested: "
+                  << RequestedRatio
+                  << ", Target:" << TargetRatio
+                  << ", Applied: " << AppliedRatio;
+    }
 };
