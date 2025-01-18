00df62f8a5cc00f4e2e000ab3addc47ee5f6bbe2
Mark Golovatskikh <m.golovatskih@gmail.com>
2025-01-18 23:33:26 +0100

    add text ttf wrapper

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 
 #include <iostream>
 
 #include "Image.h"
+#include "Text.h"
 #include "UI.h"
 #include "Window.h"
 
@@ -12,10 +14,12 @@ int main(int argc, char **argv)
     const std::string IMAGE_PATH = "assets/example.png";
     SDL_Init(SDL_INIT_VIDEO);
     IMG_Init(IMG_INIT_PNG);
+    TTF_Init();
     Window GameWindow;
     const SourceRect Src{0, 0, 200, 200};
     const DestRect Dest{0, 0, GameWindow.GetSurface()->w, GameWindow.GetSurface()->h};
     Image Example{IMAGE_PATH, Src, Dest, ScallingMode::Contain};
+    Text TextExample{"Hello world"};
     UI GameUI;
 
     SDL_Event Event;
@@ -32,11 +36,12 @@ int main(int argc, char **argv)
             }
         }
         GameWindow.Render();
-        Example.Render(GameWindow.GetSurface());
+        TextExample.Render(GameWindow.GetSurface());
         GameWindow.Update();
     }
 
     IMG_Quit();
     SDL_Quit();
+    TTF_Quit();
     return 0;
 }
