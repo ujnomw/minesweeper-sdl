066bbc5504fba64353bf80f3852cf16b443de77f
Mark Golovatskikh <m.golovatskih@gmail.com>
2025-02-09 22:41:48 +0100

    add frame time metric

--- Globals.h
+++ Globals.h
@@ -1,6 +1,7 @@
 #pragma once
 
 // #define SHOW_DEBUG_HELPERS
+// #define FRAME_PERF_DEBUG
 
 #include <SDL.h>
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 
+#include <chrono>
+
 #include "Engine/Window.h"
 #include "Globals.h"
 #include "Minesweeper/UI.h"
@@ -27,9 +29,18 @@ int main(int argc, char** argv)
 
     SDL_Event Event;
     bool shouldQuit{false};
+#ifdef FRAME_PERF_DEBUG
+    double totalFrameTime{0};
+    int frames{0};
+    double averageFrameTime;
+#endif
 
     while (!shouldQuit)
     {
+#ifdef FRAME_PERF_DEBUG
+        const auto frameStart{std::chrono::steady_clock::now()};
+#endif
+
         while (SDL_PollEvent(&Event))
         {
             if (Event.type == SDL_QUIT)
@@ -44,8 +55,16 @@ int main(int argc, char** argv)
         GameWindow.Render();
         UI.Render(GameWindow.GetSurface());
         GameWindow.Update();
+#ifdef FRAME_PERF_DEBUG
+        const auto frameEnd{std::chrono::steady_clock::now()};
+        const std::chrono::duration<double> elapsed_seconds_frame{frameEnd - frameStart};
+        totalFrameTime += elapsed_seconds_frame.count();
+        averageFrameTime = totalFrameTime / ++frames;
+        std::cout << "AFT ms: " << averageFrameTime * 1000
+                  << "; Last frame time ms: " << elapsed_seconds_frame.count() * 1000
+                  << std::endl;
+#endif
     }
-
     SDL_Quit();
     return 0;
 }
