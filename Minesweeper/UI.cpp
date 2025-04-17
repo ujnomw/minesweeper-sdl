510a758f0d2c4a7f96265d901e2f5d3314c691e8
Mark Golovatskikh <m.golovatskih@gmail.com>
2025-04-17 03:21:04 +0200

    split ui header

---
+++ Minesweeper/UI.cpp
@@ -0,0 +1,35 @@
+#include "Minesweeper/UI.h"
+
+MinesweeperUI::MinesweeperUI() { ComputeLayout(Config::PADDING, Config::PADDING); };
+void MinesweeperUI::Render(SDL_Surface* Surface) { Layout->Render(Surface); }
+
+// TODO: Make simple, once assignment and move constructors are done for layout
+// classes
+void MinesweeperUI::ComputeLayout(int i_x, int i_y)
+{
+    if (GameSettings::GetMode() == Hard)
+    {
+        Footer = std::make_unique<Engine::Layout::Row>(NewGameRow, DifficultyRow);
+        Layout = std::make_unique<Engine::Layout::Column>(Grid, *Footer.get());
+    }
+    else
+    {
+        Layout =
+            std::make_unique<Engine::Layout::Column>(Grid, NewGameRow, DifficultyRow);
+    }
+    Layout->ComputeLayout(i_x, i_y);
+}
+
+void MinesweeperUI::HandleEvent(const SDL_Event& E)
+{
+    if (E.type == UserEvents::DIFFICULTY_CHANGED)
+    {
+        ComputeLayout(Config::PADDING, Config::PADDING);
+        return;
+    }
+    Grid.HandleEvent(E);
+    Button.HandleEvent(E);
+    Counter.HandleEvent(E);
+    Switch.HandleEvent(E);
+    Label.HandleEvent(E);
+}
--- Minesweeper/UI.h
+++ Minesweeper/UI.h
@@ -23,37 +23,9 @@ class MinesweeperUI
     std::unique_ptr<Engine::Layout::Column> Layout{nullptr};
 
    public:
-    // TODO: Make simple, once assignment and move constructors are done for layout
-    // classes
-    MinesweeperUI() { ComputeLayout(Config::PADDING, Config::PADDING); };
-    void Render(SDL_Surface* Surface) { Layout->Render(Surface); }
+    MinesweeperUI();
+    void Render(SDL_Surface* Surface);
 
-    void ComputeLayout(int i_x, int i_y)
-    {
-        if (GameSettings::GetMode() == Hard)
-        {
-            Footer = std::make_unique<Engine::Layout::Row>(NewGameRow, DifficultyRow);
-            Layout = std::make_unique<Engine::Layout::Column>(Grid, *Footer.get());
-        }
-        else
-        {
-            Layout =
-                std::make_unique<Engine::Layout::Column>(Grid, NewGameRow, DifficultyRow);
-        }
-        Layout->ComputeLayout(i_x, i_y);
-    }
-
-    void HandleEvent(const SDL_Event& E)
-    {
-        if (E.type == UserEvents::DIFFICULTY_CHANGED)
-        {
-            ComputeLayout(Config::PADDING, Config::PADDING);
-            return;
-        }
-        Grid.HandleEvent(E);
-        Button.HandleEvent(E);
-        Counter.HandleEvent(E);
-        Switch.HandleEvent(E);
-        Label.HandleEvent(E);
-    }
+    void ComputeLayout(int i_x, int i_y);
+    void HandleEvent(const SDL_Event& E);
 };
